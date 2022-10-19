719bac5250411811145d17265fc6ac5609496bf0
Moshe <mobgadol@gmail.com>
2022-10-19 18:06:46 -0400

    changed positon for sections and added linear gradient bg to testimonials

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const menuBtn = document.querySelector(".navbar-toggler");
 const circle = document.querySelector(".circle");
 const drinks = document.querySelector(".thumb");
+const drinkMessage = document.querySelector(".content .textBox p");
 
 const openedMenu = () => {
   if (menuBtn.ariaExpanded === "true") {
     circle.style.top = "42.5rem";
-    drinks.style.top = "62.5rem"
+    drinks.style.top = "62.5rem";
   } else {
-    circle.style.top = "34rem"
-    drinks.style.top ="54.5rem"
+    circle.style.top = "34rem";
+    drinks.style.top = "54.5rem";
   }
-}
+};
+
+const changeMessage = (message) => {
+  drinkMessage.textContent = message;
+};
 
 const imgSlider = (image) => {
   const starbs = document.querySelector(".starbucks");
@@ -18,10 +23,8 @@ const imgSlider = (image) => {
 };
 
 const changeCircleColour = (colour) => {
-  const circle = document.querySelector(".circle");
   circle.style.transition = "0.5s ease-in-out";
   circle.style.background = colour;
 };
-
 
 menuBtn.addEventListener("click", openedMenu);
