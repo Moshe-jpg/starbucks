7fa6ccf81a27a50201c7c515ab39f240af61e872
Moshe <mobgadol@gmail.com>
2022-10-19 13:17:54 -0400

    fixed the menu open position for circle and content p

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const menuBtn = document.querySelector(".navbar-toggler");
+const circle = document.querySelector(".circle");
+
+const openedMenu = () => {
+  if (menuBtn.ariaExpanded === "true") {
+    circle.style.top = "38.5rem";
+  } else {
+    circle.style.top = "30rem"
+  }
+}
+
 const imgSlider = (image) => {
   const starbs = document.querySelector(".starbucks");
   starbs.src = image;
@@ -8,3 +19,6 @@ const changeCircleColour = (colour) => {
   circle.style.transition = "0.5s ease-in-out";
   circle.style.background = colour;
 };
+
+
+menuBtn.addEventListener("click", openedMenu);
