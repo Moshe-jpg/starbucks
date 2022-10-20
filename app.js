9aba2cf0cb3c54d467f28d28ce1f0b004b1c68ca
Moshe <mobgadol@gmail.com>
2022-10-20 17:41:18 -0400

    merged feature/animate but removed the animations

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const timeline = gsap.timeline({ defaults: { duration: 1 } });
+// const timeline = gsap.timeline({ defaults: { duration: 1 } });
 
-timeline
-  .from("header", { y: "-100", ease: "bounce", duration: 2 })
-  .from(".nav-link", { opacity: 0, stagger: 0.5 })
-  .from(
-    ".textBox h2",
-    {
-      x: "-100vw",
-      ease: "elastic",
-      duration: 2.5,
-    },
-    1
-  )
-  .from(
-    ".textBox p",
-    {
-      x: "-100vw",
-      ease: "elastic",
-      duration: 2.5,
-    },
-    1
-  )
-  .from(".imgBox", { x: "200%" }, "<.5")
-  .fromTo(
-    ".open-btn",
-    { opacity: 0, scale: 0, rotation: 720 },
-    { opacity: 1, scale: 1, rotation: 0 },
-    "<.1"
-  );
+// timeline
+//   .from("header", { y: "-100", ease: "bounce", duration: 2 })
+//   .from(".nav-link", { opacity: 0, stagger: 0.5 })
+//   .from(
+//     ".textBox h2",
+//     {
+//       x: "-100vw",
+//       ease: "elastic",
+//       duration: 2.5,
+//     },
+//     1
+//   )
+//   .from(
+//     ".textBox p",
+//     {
+//       x: "-100vw",
+//       ease: "elastic",
+//       duration: 2.5,
+//     },
+//     1
+//   )
+//   .from(".imgBox", { x: "200%" }, "<.5")
+//   .fromTo(
+//     ".open-btn",
+//     { opacity: 0, scale: 0, rotation: 720 },
+//     { opacity: 1, scale: 1, rotation: 0 },
+//     "<.1"
+//   );
 
