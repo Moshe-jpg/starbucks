4b9eebc73588e00cccaadca5dd11da7a052d7541
Moshe <mobgadol@gmail.com>
2022-10-19 22:28:51 -0400

    testing orientation lock

--- index.js
+++ index.js
@@ -27,4 +27,21 @@ const changeCircleColour = (colour) => {
   circle.style.background = colour;
 };
 
+var supportsOrientationChange = "onorientationchange" in window,
+  orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+window.addEventListener(
+  orientationEvent,
+  function () {
+    if (window.orientation == 0) {
+      document.getElementById("portrait").style.display = "";
+      document.getElementById("landscape").style.display = "none";
+    } else if (window.orientation == 90) {
+      document.getElementById("portrait").style.display = "none";
+      document.getElementById("landscape").style.display = "";
+    }
+  },
+  false
+);
+
 menuBtn.addEventListener("click", openedMenu);
